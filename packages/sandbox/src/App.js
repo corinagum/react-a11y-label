a10f771b3912b8f44e2383851902f126a2c3d3b1
Corina <14900841+corinagum@users.noreply.github.com>
2019-09-14 15:56:24 -0700

    Add aria attributes

--- packages/sandbox/src/App.js
+++ packages/sandbox/src/App.js
@@ -5,10 +5,15 @@ import ReactAccessibilityLabel from "react-a11y-label";
 function App() {
   const text = "Try out this accessible component!";
   return (
-    <main className="App">
+    <main
+      aria-live="polite"
+      aria-atomic="false"
+      aria-relevant="additions text"
+      className="App"
+    >
       <header className="App-header">
         <ReactAccessibilityLabel text={text} />
-        <p aria-hidden="true">{text + "hi"}</p>
+        <p aria-hidden="true">Hello</p>
       </header>
     </main>
   );
