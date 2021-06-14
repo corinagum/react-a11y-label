834a400b221082f50a09e2bda1b96d4d5181d4d6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-06-14 18:37:58 +0000

    Deploy corinagum/react-a11y-label to github.com/corinagum/react-a11y-label.git:gh-pages

---
+++ precache-manifest.41d3f63de1a9f1f179148591fadd61a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7430575bff6007add6fdacd65b6acf0",
+    "url": "/react-a11y-label/index.html"
+  },
+  {
+    "revision": "1295bff5dff5cad8827e",
+    "url": "/react-a11y-label/static/css/main.8b8a8a6e.chunk.css"
+  },
+  {
+    "revision": "184f8b76a035ff77d68b",
+    "url": "/react-a11y-label/static/js/2.95c6eb6f.chunk.js"
+  },
+  {
+    "revision": "1295bff5dff5cad8827e",
+    "url": "/react-a11y-label/static/js/main.0c0713eb.chunk.js"
+  },
+  {
+    "revision": "ea86653d9b59fa6fc12b",
+    "url": "/react-a11y-label/static/js/runtime~main.98540561.js"
+  }
+]);
